e5006adcb5ea847202c12104134f9c7e9524581c
crdiasneto <carlosrobertodn747@gmail.com>
2024-06-27 10:25:10 -0400

    Update Cash_Flow_Plots.py

--- Cash_Flow_Plots.py
+++ Cash_Flow_Plots.py
@@ -53,7 +53,7 @@ if ticker:  # Check if a ticker was entered
         dividend_years = dividend_per_share.index.year
 
     # Plotting using matplotlib and displaying with Streamlit
-    fig, ax1 = plt.subplots(figsize=(14, 8))
+    fig, ax1 = plt.subplots(figsize=(12, 8))
 
     ax1.set_xlabel('Year', fontsize=14)
     ax1.set_ylabel('Cash Flow Per Share', color='black', fontsize=14)
